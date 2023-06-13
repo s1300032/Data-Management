600d1b5265d34fbc447f637a132ab101f1e48c8a
s1300032 <s1300032@u-aizu.ac.jp>
2023-06-13 17:59:15 +0900

    Dice game: initial revision

---
+++ DiceGame.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class DiceGame {
+    public static void main(String[] args){
+        Random rnd = new Random();
+        int D1=rnd.nextInt(5)+1;
+        int D2=rnd.nextInt(5)+1;
+
+        System.out.println("Rolling dice...");
+        System.out.println("Die 1:"+D1);
+        System.out.println("Die 2:"+D2);
+        System.out.println("Total Value:"+(D1+D2));
+    }
+}
